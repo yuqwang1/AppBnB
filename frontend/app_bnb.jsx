d4a3de3658ad03ee6b70ae2077658ff0167865df
your name here <your_email_here>
2018-09-12 03:03:13 -0400

    auth before react router

--- frontend/app_bnb.jsx
+++ frontend/app_bnb.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { signup, login, logout } from './util/session_api_util';
+import configureStore from './store/store.js';
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+
   ReactDOM.render(<h1>Welcome to App_BnB</h1>, root);
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  window.login = login;
+  window.logout = logout;
+  window.signup = signup;
 });
---
+++ frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,17 @@
+import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from '../actions/session_actions';
+
+export const sessionErrorsReducer = (state = [], action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_ERRORS:
+      return {
+        errors: action.errors
+      };
+    case RECEIVE_CURRENT_USER:
+      return {
+        errors: []
+      };
+    default:
+      return state;
+  }
+};
